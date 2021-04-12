1009a2f751668ca73a6653d2ed4f466ed210bd79
Camilo O <coarenas1999@gmail.com>
2021-04-11 20:45:24 -0500

    Creando CRUD

---
+++ Taller_Final/Models/Entities/Cargo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taller_Final.Models.Entities
+{
+    public class Cargo
+    {
+        [Key]
+        public int CargoId { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public virtual List<EmpleadoDetalle> EmpleadoDetalles { get; set; }
+    }
+}
